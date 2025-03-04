223720b9831b67bd48509036004e969826bc27e9
Kriss <andersen19@gmail.com>
2025-03-04 14:18:41 +0100

    contact component updated
    added input fields with mat cards and some layout

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,11 +1,21 @@
+// src/app/contact/contact.component.ts
 import { Component } from '@angular/core';
-import { MatCardModule } from '@angular/material/card';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [MatCardModule, FlexLayoutModule],
+  imports: [
+    FlexLayoutModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule
+  ],
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
